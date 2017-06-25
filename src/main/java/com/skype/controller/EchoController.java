bc6fce297909422ee3d77da1ffe053447282f4e3
Anton Leliuk <a.leluk@profitsoft.ua>
2017-06-25 15:52:48 +0300

    #3

--- src/main/java/com/skype/controller/EchoController.java
+++ src/main/java/com/skype/controller/EchoController.java
@@ -2,8 +2,14 @@ package com.skype.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.social.borframework.service.ActivityProcessorCallback;
+import org.springframework.social.borframework.service.BotService;
 import org.springframework.social.botframework.api.data.Activity;
-import org.springframework.social.botframework.api.data.Attachment;
+import org.springframework.social.botframework.api.data.MediaUrl;
+import org.springframework.social.botframework.api.data.ThumbnailUrl;
+import org.springframework.social.botframework.api.data.cards.AdaptiveCard;
+import org.springframework.social.botframework.api.data.cards.AnimationCard;
+import org.springframework.social.botframework.api.data.cards.AudioCard;
 import org.springframework.social.botframework.api.data.cards.CardAction;
 import org.springframework.social.botframework.api.data.cards.CardImage;
 import org.springframework.social.botframework.api.data.cards.Fact;
@@ -12,12 +18,15 @@ import org.springframework.social.botframework.api.data.cards.ReceiptCard;
 import org.springframework.social.botframework.api.data.cards.ReceiptItem;
 import org.springframework.social.botframework.api.data.cards.SignInCard;
 import org.springframework.social.botframework.api.data.cards.ThumbnailCard;
-import org.springframework.social.botframework.api.dict.ActivityType;
+import org.springframework.social.botframework.api.data.cards.VideoCard;
+import org.springframework.social.botframework.api.data.cards.adaptiveCard.data.elements.InputText;
+import org.springframework.social.botframework.api.data.cards.adaptiveCard.data.elements.TextBlock;
+import org.springframework.social.botframework.api.data.cards.adaptiveCard.dict.CardElementType;
+import org.springframework.social.botframework.api.data.cards.adaptiveCard.dict.Color;
+import org.springframework.social.botframework.api.data.cards.adaptiveCard.dict.TextInputStyle;
 import org.springframework.social.botframework.api.dict.AttachmentLayout;
 import org.springframework.social.botframework.api.dict.CardActionType;
 import org.springframework.social.botframework.api.dict.TextFormat;
-import org.springframework.social.botframework.service.ActivityProcessorCallback;
-import org.springframework.social.botframework.service.BotService;
 import org.springframework.social.botframework.text.builder.TextBuilder;
 import org.springframework.social.botframework.text.data.Text;
 import org.springframework.social.botframework.text.dict.Smiles;
@@ -43,123 +52,170 @@ public class EchoController {
         botService.process(from, new ActivityProcessorCallback() {
             @Override
             public void onPersonalMessage(Activity activity) {
-                Activity replay = activity.createReplay().message().text(TextBuilder.newInstance().addPart(Text.text(activity.getText())).addPart(Smiles.Coffee).text());
-                botService.reply(replay);
+                Activity replyActivity = null;
+                switch (activity.getText()) {
+                    case "message":
+                        replyActivity = activity.createReplay()
+                                .message()
+                                .text(TextBuilder.newInstance().addPart(Text.text("some text")).addPart(Text.text(" ")).addPart(Smiles.Coffee).text())
+                                .textFormat(TextFormat.xml);
 
-                Activity card = activity.createReplay()
-                        .card()
-                        .attachmentLayout(AttachmentLayout.carousel)
-                        .text("Simple card")
-                        .summary("Summary of the card")
-                        .textFormat(TextFormat.xml);
+                        break;
+                    case "heroCard":
+                        replyActivity = activity.createReplay()
+                            .message()
+                            .attachmentLayout(AttachmentLayout.carousel)
+                            .text("Simple card")
+                            .summary("Summary of the card")
+                            .textFormat(TextFormat.markdown);
+                        HeroCard hc = new HeroCard()
+                            .title("Hotel Radisson Blu Hotel at Disneyland (r) Paris.")
+                            .subTitle("Booked in the last 2 hours")
+                            .text("Disneyland paris. 40 Aliee De la Mare dian Houleuse, Magny-le-Hongre, Seine-Marne.");
+                        CardImage ci = new CardImage()
+                            .url("https://dreamix.eu/blog/wp-content/uploads/2017/06/The-evolution-of-scalable-microservices-1508x706_c.jpg")
+                            .alt("hello thumb")
+                            .tap(new CardAction()
+                                    .type(CardActionType.openUrl)
+                                    .value("https://en.wikipedia.org/wiki/Space_Needle")
+                                    .title("open"));
+                        hc.image(ci);
 
-                HeroCard hc = new HeroCard()
-                        .title("Hotel Radisson Blu Hotel at Disneyland (r) Paris.")
-                        .subTitle("Booked in the last 2 hours")
-                        .text("Disneyland paris. 40 Aliee De la Mare dian Houleuse, Magny-le-Hongre, Seine-Marne.");
-                CardImage ci = new CardImage()
-                        .url("https://pp.vk.me/c7011/v7011856/29c82/BvJogtnQIfE.jpg")
-                        .alt("hello thumb")
-                        .tap(new CardAction()
-                                .type(CardActionType.openUrl)
-                                .value("https://en.wikipedia.org/wiki/Space_Needle")
-                                .title("open"));
-                hc.addImage(ci);
+                        hc.button(new CardAction().title("Select").value("select:102").type(CardActionType.imBack));
 
-                hc.addButton(new CardAction().title("Select").value("select:102").type(CardActionType.imBack));
+                        replyActivity.addAttachment(hc.toAttachment());
+                        break;
+                    case "signinCard":
+                        replyActivity = activity.createReplay()
+                            .message()
+                            .attachmentLayout(AttachmentLayout.carousel)
+                            .text("Simple signIn card")
+                            .summary("Summary of the signIn card")
+                            .textFormat(TextFormat.xml);
 
-                card.addAttachment(hc.toAttachment());
-                botService.reply(card);
+                        SignInCard sc = new SignInCard().title("Sign title").text("Some text").subTitle("Some subtitle");
 
-                card = activity.createReplay()
-                        .card()
-                        .attachmentLayout(AttachmentLayout.carousel)
-                        .text("Simple signIn card")
-                        .summary("Summary of the signIn card")
-                        .textFormat(TextFormat.xml);
+                        CardAction signInButton = new CardAction()
+                            .title("Login in TheSystem")
+                            .type(CardActionType.signin)
+                            .value("https://profitsoft.ua/uathesystem");
+                        sc.button(signInButton);
+                        replyActivity.addAttachment(sc.toAttachment());
+                        break;
+                    case "thumbnailCard":
+                        replyActivity = activity.createReplay()
+                            .message()
+                            .attachmentLayout(AttachmentLayout.carousel)
+                            .text("Simple thumbnail card")
+                            .summary("Summary of the thumbnail card")
+                            .textFormat(TextFormat.xml);
 
-                SignInCard sc = new SignInCard().title("Sign title").text("Some text").subTitle("Some subtitle");
+                        ThumbnailCard tc = new ThumbnailCard().title("Title").text("Text").subTitle("Sub title");
 
-                CardAction signInButton = new CardAction()
-                        .title("Login in TheSystem")
-                        .type(CardActionType.signin)
-                        .value("https://profitsoft.ua/uathesystem");
-                sc.addButton(signInButton);
-                card.addAttachment(sc.toAttachment());
-                botService.reply(card);
+                        CardImage tcImage = new CardImage()
+                                .alt("Thumbnail image")
+                                .url("https://dreamix.eu/blog/wp-content/uploads/2017/06/The-evolution-of-scalable-microservices-1508x706_c.jpg")
+                                .tap(new CardAction().value("https://dreamix.eu/blog/wp-content/uploads/2017/06/The-evolution-of-scalable-microservices-1508x706_c.jpg").type(CardActionType.openUrl));
+                        tc.image(tcImage);
 
-                card = activity.createReplay()
-                        .card()
-                        .attachmentLayout(AttachmentLayout.carousel)
-                        .text("Simple thumbnail card")
-                        .summary("Summary of the thumbnail card")
-                        .textFormat(TextFormat.xml);
+                        replyActivity.addAttachment(tc.toAttachment());
+                        break;
+                    case "receiptCard":
+                            replyActivity = activity.createReplay()
+                                    .attachmentLayout(AttachmentLayout.carousel)
+                                    .message()
+                                    .text("Simple receipt card")
+                                    .summary("Summary of the receipt card")
+                                    .textFormat(TextFormat.xml);
 
-                ThumbnailCard tc = new ThumbnailCard().title("Title").text("Text").subTitle("Sub title");
+                            ReceiptCard rc = new ReceiptCard().title("Title").text("Text").subTitle("Sub title");
 
-                CardAction tcButton = new CardAction();
-                tcButton.setType(CardActionType.openUrl);
-                tcButton.setTitle("view");
-                tcButton.setValue("https://pp.vk.me/c7011/v7011856/29c82/BvJogtnQIfE.jpg");
-                tc.getButtons().add(tcButton);
+                            rc.fact(new Fact().key("Some key").value("Some value"));
 
-                Attachment<ThumbnailCard> ta = new Attachment<>();
-                ta.setContent(tc);
-                ta.setContentType(tc.getCardType().getType());
-                card.addAttachment(ta);
-                botService.reply(card);
+                            ReceiptItem ri = new ReceiptItem();
+                            CardImage rci = new CardImage()
+                                .url("https://dreamix.eu/blog/wp-content/uploads/2017/06/The-evolution-of-scalable-microservices-1508x706_c.jpg")
+                                .alt("alt text");
+                            ri.image(rci).quantity(1).price("100 uah");
+                            rc.item(ri).tax("TAX: 150 uah").vat("VAT: 200 uah").total("TOTAL: 350 uah");
 
-                card = activity.createReplay();
-                card.setAttachmentLayout(AttachmentLayout.carousel);
-                card.setType(ActivityType.card);
-                card.setText("Simple receipt card");
-                card.setSummary("Summary of the receipt card");
-                card.setTextFormat(TextFormat.xml);
+                        replyActivity.addAttachment(rc.toAttachment());
+                        break;
+                    case "videoCard":
+                        replyActivity = activity.createReplay()
+                                .message()
+                                .text("Video")
+                                .summary("Some card with video");
+                        VideoCard vc = new VideoCard()
+                                .title("Big Buck Bunny")
+                                .subTitle("by the Blender Institute")
+                                .autoStart(false)
+                                .text("Big Buck Bunny (code-named Peach) is a short computer-animated comedy film by the Blender Institute, part of the Blender Foundation. Like the foundation\\'s previous film Elephants Dream, the film was made using Blender, a free software application for animation made by the same foundation. It was released as an open-source film under Creative Commons License Attribution 3.0.")
+                                .image(new ThumbnailUrl().url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Big_buck_bunny_poster_big.jpg/220px-Big_buck_bunny_poster_big.jpg").alt("alt text"))
+                                .media(new MediaUrl().url("http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4").profile("Media profile"))
+                                .button(new CardAction().type(CardActionType.openUrl).value("https://peach.blender.org/").title("Learn More"));
+                        replyActivity.addAttachment(vc.toAttachment());
+                        break;
+                    case "audioCard":
+                        replyActivity = activity.createReplay()
+                                .message()
+                                .text("Audio")
+                                .summary("Some card with audio");
+                        AudioCard ac = new AudioCard()
+                                .title("I am your father")
+                                .subTitle("Star Wars: Episode V - The Empire Strikes Back")
+                                .text("The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.")
+                                .autoStart(false)
+                                .image(new ThumbnailUrl().url("https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg").alt("alt text"))
+                                .media(new MediaUrl().url("http://www.wavlist.com/movies/004/father.wav"))
+                                .button(new CardAction().type(CardActionType.openUrl).value("https://en.wikipedia.org/wiki/The_Empire_Strikes_Back").title("Read More"));
+                        replyActivity.addAttachment(ac.toAttachment());
+                        break;
+                    case "animationCard":
+                        replyActivity = activity.createReplay()
+                                .message()
+                                .text("Animation")
+                                .summary("Some card with animation");
+                        AnimationCard anc = new AnimationCard()
+                                .title("Animation card")
+                                .subTitle("Some sub title")
+                                .image(new ThumbnailUrl().alt("alt text").url("https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png"))
+                                .media(new MediaUrl().url("http://i.giphy.com/Ki55RUbOV5njy.gif").profile("Cool animation"));
+                        replyActivity.addAttachment(anc.toAttachment());
+                        break;
+                    case "adaptiveCard":
+                        replyActivity = activity.createReplay()
+                                .message()
+                                .text("Adaptive card")
+                                .summary("All adaptive cards");
+                        AdaptiveCard adc = new AdaptiveCard()
+                                .text("Card test")
+                                .subTitle("Test adaptive card")
+                                .type(CardElementType.AdaptiveCard)
+                                .fallbackText("Client doesn't support adaptive cards.")
+                                .body(new TextBlock().text("Text of block").color(Color.ATTENTION))
+                                .body(new InputText().multiLine(false).maxLength(100).style(TextInputStyle.text));
+                        replyActivity.addAttachment(adc.toAttachment());
+                        break;
+                    default:
+                        replyActivity = activity.createReplay().message().text("Available commands: message, heroCard, signinCard, thumbnailCard, receiptCard, videoCard, audioCard, animationCard, adaptiveCard.");
+                        break;
+                }
 
-                ReceiptCard rc = new ReceiptCard();
-                rc.setTitle("Title");
-                rc.setText("Text");
-                rc.setSubTitle("Sub title");
-
-                rc.addFact(new Fact().key("Some key").value("Some value"));
-
-                ReceiptItem ri = new ReceiptItem();
-                CardImage rci = new CardImage();
-                rci.setUrl("https://pp.vk.me/c7011/v7011856/29c82/BvJogtnQIfE.jpg");
-                rci.setAlt("alt text");
-                ri.setImage(rci);
-                ri.setQuantity(1);
-                ri.setPrice("100 uah");
-                rc.addItem(ri);
-                rc.setTax("TAX: 150 uah");
-                rc.setVat("VAT: 200 uah");
-                rc.setTotal("TOTAL: 350 uah");
-
-                Attachment<ReceiptCard> ra = new Attachment<>();
-                ra.setContent(rc);
-                ra.setContentType(rc.getCardType().getType());
-                card.addAttachment(ra);
-
-                botService.reply(card);
+                botService.reply(replyActivity);
             }
 
             @Override
-            public void onGroupMessage(Activity activity) {}
-
-            @Override
-            public void onContactRelationUpdate(Activity activity) {}
-
-            @Override
-            public void onConversationUpdate(Activity activity) {}
-
-            @Override
-            public void onTyping(Activity activity) {}
-
-            @Override
-            public void onPing(Activity activity) {}
-
-            @Override
-            public void onDeleteUserData(Activity activity) {}
+            public void onConversationUpdate(Activity activity) {
+                activity.getMembersAdded().forEach(m -> {
+                    Activity welcome = activity.createReplay()
+                            .message()
+                            .textFormat(TextFormat.xml)
+                            .text("Hello " + m.getName() + ". " +
+                                    "Available commands: message, heroCard, signinCard, thumbnailCard, receiptCard, videoCard, audioCard, animationCard, adaptiveCard.");
+                    botService.reply(welcome);
+                });
+            }
         });
     }
 }
